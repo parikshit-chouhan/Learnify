0c36581f68fa34d257c699bba43e0d5583493da9
unknown <parikshitchouhan1076@gmail.com>
2025-01-04 14:00:33 +0530

    change url

--- server/index.js
+++ server/index.js
@@ -21,10 +21,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}));
+// app.use(cors({
+//     origin:"http://localhost:5173",
+//     credentials:true
+// }));
 
 app.use(
     cors({
